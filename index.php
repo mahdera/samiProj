3186b72e1d128eefddfd8b751583a3a44aee68d0
Mahder Neway <alemayehu@Mahders-MacBook-Pro.local>
2014-09-06 09:51:44 -0400

    modified the file again

--- index.php
+++ index.php
@@ -7,13 +7,105 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Sami's Project</title>
+        <script type='text/javascript' src="js/jquery-1.11.1.js"></script>
     </head>
     <body>
         <table border="1" width="100%">
             <tr>
-                <td></td>
+                <td><a href="#.php" id="teamManagementLink">Team</a></td>
+                <td><a href="#.php" id="responsibilityManagementLink">Responsibility</a></td>
+                <td><a href="#.php" id="assessmentManagementLink">Assessment</a></td>
+                <td><a href="#.php" id="thManagementLink">TH</a></td>
+                <td><a href="#.php" id="riskManagementLink">Risk</a></td>
+                <td><a href="#.php" id="goalFirstManagementLink">Goal First</a></td>
+                <td><a href="#.php" id="goalSecondManagementLink">Goal Second</a></td>
+                <td><a href="#.php" id="putThActionManagementLink">Put TH Action</a></td>
+                <td><a href="#.php" id="putFnActionManagementLink">Put Fn Action</a></td>
+                <td><a href="#.php" id="editThActionManagementLink">Edit TH Action</a></td>
+                <td><a href="#.php" id="editFnActionManagementLink">Edit Fn Action</a></td>
+                <td><a href="#.php" id="form1ManagementLink">Form 1</a></td>
+                <td><a href="#.php" id="form2ManagementLink">Form 2</a></td>
+                <td><a href="#.php" id="form3ManagementLink">Form 3</a></td>
+                <td><a href="#.php" id="form4ManagementLink">Form 4</a></td>
+                <td><a href="#.php" id="form5ManagementLink">Form 5</a></td>
+                <td><a href="#.php" id="form6ManagementLink">Form 6</a></td>
+                <td><a href="#.php" id="form7ManagementLink">Form 7</a></td>
+                <td><a href="#.php" id="form8ManagementLink">Form 8</a></td>
+                <td><a href="#.php" id="form9ManagementLink">Form 9</a></td>
+                <td><a href="#.php" id="form10ManagementLink">Form 10</a></td>
+                <td><a href="#.php" id="reportManagementLink">Report</a></td>
             </tr>
         </table>
+        
+        <div id="mainDetailDiv">            
+        </div>
+        
     </body>
 </html>
+<script type='text/javascript'>
+    $(document).ready(function(){
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        $('#').click(function(){});
+        
+        
+        
+        
+    });//end document.ready function
+</script>
