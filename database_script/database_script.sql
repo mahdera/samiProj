7e2bf8f280ae5e131d1a85c80442d5c4ae5547fd
Mahder Neway <mahdera@yahoo.com>
2014-10-03 21:42:46 -0400

    modified the db script

--- database_script/database_script.sql
+++ database_script/database_script.sql
@@ -1,20 +1,18 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 3.3.9.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Sep 25, 2014 at 02:19 PM
--- Server version: 5.5.34
--- PHP Version: 5.5.10
+-- Host: localhost
+-- Generation Time: Oct 03, 2014 at 09:41 PM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Database: `db_sami_proj`
 --
-create database db_sami_proj;
-use db_sami_proj;
+
 -- --------------------------------------------------------
 
 --
@@ -27,14 +25,12 @@ CREATE TABLE `tbl_assessment` (
   `assessment_date` date NOT NULL,
   `summary` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `tbl_assessment`
 --
 
-INSERT INTO `tbl_assessment` (`id`, `assessment_type`, `assessment_date`, `summary`) VALUES
-(3, 'as2', '2014-09-11', 'This is the summary Information done by Mahder.');
 
 -- --------------------------------------------------------
 
@@ -49,18 +45,12 @@ CREATE TABLE `tbl_assessment_th` (
   PRIMARY KEY (`id`),
   KEY `assessment_id` (`assessment_id`),
   KEY `th_id` (`th_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `tbl_assessment_th`
 --
 
-INSERT INTO `tbl_assessment_th` (`id`, `assessment_id`, `th_id`) VALUES
-(1, 3, 7),
-(2, 3, 8),
-(3, 3, 9),
-(4, 3, 10),
-(5, 3, 11);
 
 -- --------------------------------------------------------
 
@@ -72,15 +62,16 @@ CREATE TABLE `tbl_fn` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `fn_name` varchar(70) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `tbl_fn`
 --
 
-INSERT INTO `tbl_fn` (`id`, `fn_name`) VALUES
-(1, 'mahder the fn good'),
-(2, 'this is good');
+INSERT INTO `tbl_fn` VALUES(1, 'sdfdsf');
+INSERT INTO `tbl_fn` VALUES(2, 'sdfdf');
+INSERT INTO `tbl_fn` VALUES(3, 'dfsf');
+INSERT INTO `tbl_fn` VALUES(4, 'this is mahder');
 
 -- --------------------------------------------------------
 
@@ -94,7 +85,13 @@ CREATE TABLE `tbl_fn_action` (
   `action_text` text,
   PRIMARY KEY (`id`),
   KEY `fn_id` (`fn_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_fn_action`
+--
+
+INSERT INTO `tbl_fn_action` VALUES(1, 4, 'Bill Cosby Action. I love this guy');
 
 -- --------------------------------------------------------
 
@@ -107,14 +104,17 @@ CREATE TABLE `tbl_goal_first` (
   `th_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `th_id` (`th_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_goal_first`
 --
 
-INSERT INTO `tbl_goal_first` (`id`, `th_id`) VALUES
-(1, 12);
+INSERT INTO `tbl_goal_first` VALUES(5, 4);
+INSERT INTO `tbl_goal_first` VALUES(1, 5);
+INSERT INTO `tbl_goal_first` VALUES(2, 5);
+INSERT INTO `tbl_goal_first` VALUES(3, 5);
+INSERT INTO `tbl_goal_first` VALUES(4, 5);
 
 -- --------------------------------------------------------
 
@@ -126,17 +126,20 @@ CREATE TABLE `tbl_goal_first_g1` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_id` bigint(20) NOT NULL,
   `g1` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_id` (`goal_first_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_goal_first_g1`
 --
 
-INSERT INTO `tbl_goal_first_g1` (`id`, `goal_first_id`, `g1`, `fn`) VALUES
-(1, 1, 'g1', 'mahder the fn good');
+INSERT INTO `tbl_goal_first_g1` VALUES(1, 1, 'hgjhg', 1);
+INSERT INTO `tbl_goal_first_g1` VALUES(2, 1, 'lskdjf', 2);
+INSERT INTO `tbl_goal_first_g1` VALUES(3, 1, 'hgjh', 3);
+INSERT INTO `tbl_goal_first_g1` VALUES(4, 1, 'jhghg', 4);
+INSERT INTO `tbl_goal_first_g1` VALUES(5, 5, 'This is G1', 4);
 
 -- --------------------------------------------------------
 
@@ -148,10 +151,21 @@ CREATE TABLE `tbl_goal_first_g1_obj_fn` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_g1_id` bigint(20) NOT NULL,
   `obj` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_g1_id` (`goal_first_g1_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tbl_goal_first_g1_obj_fn`
+--
+
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(1, 2, '', 1);
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(2, 2, '', 1);
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(3, 3, 'jkhkjiu', 2);
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(4, 3, 'kjkjh', 2);
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(5, 4, 'hkjh', 3);
+INSERT INTO `tbl_goal_first_g1_obj_fn` VALUES(6, 4, 'yuhjg', 4);
 
 -- --------------------------------------------------------
 
@@ -163,17 +177,20 @@ CREATE TABLE `tbl_goal_first_g2` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_id` bigint(20) NOT NULL,
   `g2` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_id` (`goal_first_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_goal_first_g2`
 --
 
-INSERT INTO `tbl_goal_first_g2` (`id`, `goal_first_id`, `g2`, `fn`) VALUES
-(1, 1, 'g2', 'this is good');
+INSERT INTO `tbl_goal_first_g2` VALUES(1, 1, 'jhhgf', 1);
+INSERT INTO `tbl_goal_first_g2` VALUES(2, 1, 'dlfkjdklj', 2);
+INSERT INTO `tbl_goal_first_g2` VALUES(3, 1, 'hkjhj', 3);
+INSERT INTO `tbl_goal_first_g2` VALUES(4, 1, 'hjgjhg', 4);
+INSERT INTO `tbl_goal_first_g2` VALUES(5, 5, 'This is G2', 2);
 
 -- --------------------------------------------------------
 
@@ -185,10 +202,21 @@ CREATE TABLE `tbl_goal_first_g2_obj_fn` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_g2_id` bigint(20) NOT NULL,
   `obj` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_g2_id` (`goal_first_g2_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tbl_goal_first_g2_obj_fn`
+--
+
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(1, 2, '', 1);
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(2, 2, '', 2);
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(3, 3, 'kjhjkh', 3);
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(4, 3, 'ijkhjk', 4);
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(5, 4, 'kjhjkh', 1);
+INSERT INTO `tbl_goal_first_g2_obj_fn` VALUES(6, 4, 'ijkhjk', 2);
 
 -- --------------------------------------------------------
 
@@ -200,17 +228,20 @@ CREATE TABLE `tbl_goal_first_g3` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_id` bigint(20) NOT NULL,
   `g3` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_id` (`goal_first_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_goal_first_g3`
 --
 
-INSERT INTO `tbl_goal_first_g3` (`id`, `goal_first_id`, `g3`, `fn`) VALUES
-(1, 1, 'g3', 'this is good');
+INSERT INTO `tbl_goal_first_g3` VALUES(1, 1, 'kguhg', 1);
+INSERT INTO `tbl_goal_first_g3` VALUES(2, 1, 'dfkljdkjf', 2);
+INSERT INTO `tbl_goal_first_g3` VALUES(3, 1, 'iyuiu', 3);
+INSERT INTO `tbl_goal_first_g3` VALUES(4, 1, 'gjhghg', 4);
+INSERT INTO `tbl_goal_first_g3` VALUES(5, 5, 'This is G3', 1);
 
 -- --------------------------------------------------------
 
@@ -222,17 +253,21 @@ CREATE TABLE `tbl_goal_first_g3_obj_fn` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goal_first_g3_id` bigint(20) NOT NULL,
   `obj` varchar(50) NOT NULL,
-  `fn` varchar(50) NOT NULL,
+  `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_first_g3_id` (`goal_first_g3_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `tbl_goal_first_g3_obj_fn`
 --
 
-INSERT INTO `tbl_goal_first_g3_obj_fn` (`id`, `goal_first_g3_id`, `obj`, `fn`) VALUES
-(1, 1, 'trevor obj', 'this is good');
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(1, 2, '', 1);
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(2, 2, '', 2);
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(3, 3, '', 3);
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(4, 3, '', 4);
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(5, 4, 'madew', 1);
+INSERT INTO `tbl_goal_first_g3_obj_fn` VALUES(6, 4, 'lkhj', 2);
 
 -- --------------------------------------------------------
 
@@ -245,7 +280,21 @@ CREATE TABLE `tbl_goal_second` (
   `fn_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fn_id` (`fn_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `tbl_goal_second`
+--
+
+INSERT INTO `tbl_goal_second` VALUES(2, 1);
+INSERT INTO `tbl_goal_second` VALUES(4, 1);
+INSERT INTO `tbl_goal_second` VALUES(7, 1);
+INSERT INTO `tbl_goal_second` VALUES(3, 3);
+INSERT INTO `tbl_goal_second` VALUES(6, 3);
+INSERT INTO `tbl_goal_second` VALUES(1, 4);
+INSERT INTO `tbl_goal_second` VALUES(5, 4);
+INSERT INTO `tbl_goal_second` VALUES(8, 4);
+INSERT INTO `tbl_goal_second` VALUES(9, 4);
 
 -- --------------------------------------------------------
 
@@ -259,7 +308,20 @@ CREATE TABLE `tbl_goal_second_g1` (
   `g1` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_id` (`goal_second_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g1`
+--
+
+INSERT INTO `tbl_goal_second_g1` VALUES(1, 1, 'a');
+INSERT INTO `tbl_goal_second_g1` VALUES(2, 2, 'df');
+INSERT INTO `tbl_goal_second_g1` VALUES(3, 3, 'sdf');
+INSERT INTO `tbl_goal_second_g1` VALUES(4, 1, 'eg');
+INSERT INTO `tbl_goal_second_g1` VALUES(5, 3, 'sadf');
+INSERT INTO `tbl_goal_second_g1` VALUES(6, 2, 'dfg');
+INSERT INTO `tbl_goal_second_g1` VALUES(7, 1, 'd');
+INSERT INTO `tbl_goal_second_g1` VALUES(8, 1, 'g1');
 
 -- --------------------------------------------------------
 
@@ -273,7 +335,17 @@ CREATE TABLE `tbl_goal_second_g1_obj` (
   `obj` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_g1_id` (`goal_second_g1_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g1_obj`
+--
+
+INSERT INTO `tbl_goal_second_g1_obj` VALUES(1, 4, 'erg');
+INSERT INTO `tbl_goal_second_g1_obj` VALUES(2, 5, 'erg');
+INSERT INTO `tbl_goal_second_g1_obj` VALUES(3, 5, 'tuk');
+INSERT INTO `tbl_goal_second_g1_obj` VALUES(4, 8, 'g1 added obj');
+INSERT INTO `tbl_goal_second_g1_obj` VALUES(5, 8, 'g1 added obj');
 
 -- --------------------------------------------------------
 
@@ -287,7 +359,18 @@ CREATE TABLE `tbl_goal_second_g2` (
   `g2` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_id` (`goal_second_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g2`
+--
+
+INSERT INTO `tbl_goal_second_g2` VALUES(1, 2, 'fds');
+INSERT INTO `tbl_goal_second_g2` VALUES(2, 1, 'tyrj');
+INSERT INTO `tbl_goal_second_g2` VALUES(3, 3, 'gf');
+INSERT INTO `tbl_goal_second_g2` VALUES(4, 2, 'erthg');
+INSERT INTO `tbl_goal_second_g2` VALUES(5, 1, 'f');
+INSERT INTO `tbl_goal_second_g2` VALUES(6, 1, 'g2');
 
 -- --------------------------------------------------------
 
@@ -301,7 +384,17 @@ CREATE TABLE `tbl_goal_second_g2_obj` (
   `obj` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_g2_id` (`goal_second_g2_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g2_obj`
+--
+
+INSERT INTO `tbl_goal_second_g2_obj` VALUES(1, 2, 'aefgwre');
+INSERT INTO `tbl_goal_second_g2_obj` VALUES(2, 3, 'ghj');
+INSERT INTO `tbl_goal_second_g2_obj` VALUES(3, 3, 'tyu');
+INSERT INTO `tbl_goal_second_g2_obj` VALUES(4, 6, 'g2 added obj');
+INSERT INTO `tbl_goal_second_g2_obj` VALUES(5, 6, 'g2 added obj');
 
 -- --------------------------------------------------------
 
@@ -315,7 +408,14 @@ CREATE TABLE `tbl_goal_second_g3` (
   `g3` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_id` (`goal_second_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g3`
+--
+
+INSERT INTO `tbl_goal_second_g3` VALUES(1, 2, 'vsdv');
+INSERT INTO `tbl_goal_second_g3` VALUES(2, 1, 'g3');
 
 -- --------------------------------------------------------
 
@@ -329,7 +429,16 @@ CREATE TABLE `tbl_goal_second_g3_obj` (
   `obj` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `goal_second_g3_id` (`goal_second_g3_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tbl_goal_second_g3_obj`
+--
+
+INSERT INTO `tbl_goal_second_g3_obj` VALUES(1, 1, 'rht');
+INSERT INTO `tbl_goal_second_g3_obj` VALUES(4, 1, 'sdf');
+INSERT INTO `tbl_goal_second_g3_obj` VALUES(5, 2, 'g3 added obj');
+INSERT INTO `tbl_goal_second_g3_obj` VALUES(6, 2, 'g3 added obj');
 
 -- --------------------------------------------------------
 
@@ -344,14 +453,14 @@ CREATE TABLE `tbl_responsibility` (
   `responsibility` text,
   PRIMARY KEY (`id`),
   KEY `team_id` (`team_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `tbl_responsibility`
 --
 
-INSERT INTO `tbl_responsibility` (`id`, `team_id`, `role`, `responsibility`) VALUES
-(1, 4, 'asdfasdf', 'asfgasdfgsd');
+INSERT INTO `tbl_responsibility` VALUES(1, 2, 'This is the role description', 'this is the responsibility textarea');
+INSERT INTO `tbl_responsibility` VALUES(2, 1, 'This is the second role', 'This is the second responsibility');
 
 -- --------------------------------------------------------
 
@@ -369,14 +478,12 @@ CREATE TABLE `tbl_risk` (
   `rs` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `th_id` (`th_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `tbl_risk`
 --
 
-INSERT INTO `tbl_risk` (`id`, `th_id`, `mg`, `dr`, `pr`, `wa`, `rs`) VALUES
-(1, 12, 'mg2', 'dr3', 'pr2', 'wa3', 'rs3');
 
 -- --------------------------------------------------------
 
@@ -393,23 +500,33 @@ CREATE TABLE `tbl_team` (
   `phone` varchar(70) NOT NULL,
   `interest` varchar(70) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `tbl_team`
 --
 
-INSERT INTO `tbl_team` (`id`, `team_name`, `title`, `organization`, `email`, `phone`, `interest`) VALUES
-(3, 'team 1', 'title 1', 'organization 1', 'email1@yahoo.com', '9485749875', '3'),
-(4, 'dfg', 'adsfg', 'adfg', 'ghsfgdh@df.com', '4563456', '3'),
-(5, '', '', '', '', '', ''),
-(6, '', '', '', '', '', ''),
-(7, '', '', '', '', '', ''),
-(8, '', '', '', '', '', ''),
-(9, '', '', '', '', '', ''),
-(10, '', '', '', '', '', ''),
-(11, '', '', '', '', '', ''),
-(12, '', '', '', '', '', '');
+INSERT INTO `tbl_team` VALUES(1, 'Name', 'title', 'organization', 'mahdera@yahoo.com', '2020039494', '3');
+INSERT INTO `tbl_team` VALUES(2, 'sdf', 'lksdjfk', 'lkdj', 'lekbirgebre@yahoo.com', '4985723984', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_team_interest`
+--
+
+CREATE TABLE `tbl_team_interest` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `team_id` bigint(20) NOT NULL,
+  `interest_name` varchar(70) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `team_id` (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `tbl_team_interest`
+--
+
 
 -- --------------------------------------------------------
 
@@ -421,21 +538,17 @@ CREATE TABLE `tbl_th` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `th_name` varchar(70) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_th`
 --
 
-INSERT INTO `tbl_th` (`id`, `th_name`) VALUES
-(7, 'Th1'),
-(8, 'Th2'),
-(9, 'Th3'),
-(10, 'Th4'),
-(11, 'Th5'),
-(12, 'Nitendo Games'),
-(13, ''),
-(14, '');
+INSERT INTO `tbl_th` VALUES(1, 'sdff');
+INSERT INTO `tbl_th` VALUES(2, 'dfgdfg');
+INSERT INTO `tbl_th` VALUES(3, 'undefined');
+INSERT INTO `tbl_th` VALUES(4, 'A TH Name');
+INSERT INTO `tbl_th` VALUES(5, 'Another TH');
 
 -- --------------------------------------------------------
 
@@ -449,7 +562,13 @@ CREATE TABLE `tbl_th_action` (
   `action_text` text,
   PRIMARY KEY (`id`),
   KEY `th_id` (`th_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_th_action`
+--
+
+INSERT INTO `tbl_th_action` VALUES(1, 5, 'This is the action done for Th action by Mahder Neway. This line is updated.');
 
 --
 -- Constraints for dumped tables
@@ -563,6 +682,12 @@ ALTER TABLE `tbl_responsibility`
 --
 ALTER TABLE `tbl_risk`
   ADD CONSTRAINT `tbl_risk_ibfk_1` FOREIGN KEY (`th_id`) REFERENCES `tbl_th` (`id`);
+
+--
+-- Constraints for table `tbl_team_interest`
+--
+ALTER TABLE `tbl_team_interest`
+  ADD CONSTRAINT `tbl_team_interest_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `tbl_team` (`id`);
 
 --
 -- Constraints for table `tbl_th_action`
