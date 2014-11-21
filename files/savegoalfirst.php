90df177ee56217ce6237483cf650244e33c58623
Mahder Neway <mahdera@yahoo.com>
2014-11-21 08:39:56 -0500

    fix for getting the report as per the new database structure...

--- files/savegoalfirst.php
+++ files/savegoalfirst.php
@@ -31,16 +31,15 @@
     //if there is no goal first record by this particular user.
     $fetchedGoalFirstRecord = getGoalFirstUsingModifiedBy($_SESSION['LOGGED_USER_ID']);
     $dateDifference = getDateDifferenceForGoalFirstUsingModifiedBy($_SESSION['LOGGED_USER_ID']);
+    echo 'the date diff is : ' . $dateDifference.'<br/>';
 
-    if($dateDifference){
+    if($dateDifference >= 0){
         if($dateDifference > 180){
             //last created goalFirst record is older than 6 months...hence create new...
             $_SESSION['GOAL_FIRST_STATUS'] = 'create';
         }else{
             $_SESSION['GOAL_FIRST_STATUS'] = 'existing';
         }
-    }else{
-        $_SESSION['GOAL_FIRST_STATUS'] = 'create';
     }
 
     if($_SESSION['GOAL_FIRST_STATUS'] == 'create'){
