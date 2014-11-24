b05b1b24be317c691a8d0ee0952756cf62a3ad17
Mahder Neway <mahdera@yahoo.com>
2014-11-23 20:51:22 -0500

    save modification has been updated so that a default 0 value is passed when creating function from other users perspective

--- files/savefn.php
+++ files/savefn.php
@@ -2,6 +2,6 @@
     session_start();
     require_once 'fn.php';
     @$fnName = mysql_real_escape_string($_POST['fnName']);
-    saveFn($fnName, $_SESSION['LOGGED_USER_ID'],false);
+    saveFn($fnName, $_SESSION['LOGGED_USER_ID'],0);
 ?>
 <div class="notify notify-green"><span class="symbol icon-tick"></span> Fn saved successfully!</div>
