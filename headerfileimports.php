701211d14dd017d3abd4bbb5ad05bf145fafe9e3
Mahder Neway <mahder.neway@echoorigin.com>
2014-12-11 14:14:56 -0500

    Successfully integreated the event calendar to the project

--- headerfileimports.php
+++ headerfileimports.php
@@ -1,10 +1,40 @@
-<link rel='stylesheet' type='text/css' href='libs/css/smoothness/jquery-ui-1.8rc3.custom.css' />
-<link rel='stylesheet' type='text/css' href='css/jquery.weekcalendar.css' />
-<link rel='stylesheet' type='text/css' href='css/calendar.css' />
-<link rel='stylesheet' type='text/css' href='css/reset.css' />
-<link rel='stylesheet' type='text/css' href='css/engine.css' />
+<link rel='stylesheet' type='text/css' href='calendar/libs/css/smoothness/jquery-ui-1.8rc3.custom.css' />
+<link rel='stylesheet' type='text/css' href='calendar/css/jquery.weekcalendar.css' />
+<link rel='stylesheet' type='text/css' href='calendar/css/calendar.css' />
+<link rel='stylesheet' type='text/css' href='calendar/css/reset.css' />
+<link rel='stylesheet' type='text/css' href='calendar/css/engine.css' />
 
-<script type='text/javascript' src="libs/jquery-1.4.2.min.js"></script>
-<script type='text/javascript' src='libs/jquery-ui-1.8rc3.custom.min.js'></script>
-<script type='text/javascript' src='js/jquery.weekcalendar.js'></script>
-<script type='text/javascript' src='js/engine.js'></script>
+<script type='text/javascript' src="calendar/libs/jquery-1.4.2.min.js"></script>
+<script type='text/javascript' src='calendar/libs/jquery-ui-1.8rc3.custom.min.js'></script>
+<script type='text/javascript' src='calendar/js/jquery.weekcalendar.js'></script>
+<script type='text/javascript' src='calendar/js/engine.js'></script>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#logoutLink').click(function(){
+      var txt = 'Are you sure you want to logout?';
+
+      $.prompt(txt,{
+        buttons:{Logout:true, Cancel:false},
+        close: function(e,v,m,f){
+
+          if(v){
+            $.ajax({
+              url: 'logout.php',
+              data: null,
+              type:'POST',
+              success:function(response){
+                window.location = 'login.php';
+              },
+              error:function(error){
+                alert(error);
+              }
+            });
+          }
+          else{}
+          }
+        });
+
+      });
+  });//end document.ready function
+</script>
