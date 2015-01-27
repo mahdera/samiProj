c1b9cb4107268b87201eb8e0ae526f84a72e71b3
Mahder Neway <mneway@mahder01.local>
2015-01-27 14:07:07 -0500

    added sample unit test cases

--- tests/TestCalculator.php
+++ tests/TestCalculator.php
@@ -4,7 +4,7 @@
   class TestCalculator extends PHPUnit_Framework_TestCase{
     public function testAdd(){
       $a = new Calculator();
-      $this->assertEquals(6, $a->add(4,2));
+      $this->assertEquals(6, $a->add(4,4));
     }
   }//end class
 ?>
